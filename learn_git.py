d468b869afffbcdccd4126f5dc8f1ef55a53a48e
HuanGE <395564826@qq.com>
2019-02-16 15:32:55 +0800

    create a file

---
+++ learn_git.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019-2-16 15:25
+# @Author  : GongLH
+# @Email   : gonglinhuan@gmail.com
+# @File    : learn_git.py
+# @Software: PyCharm
+
+import this
+
+if __name__ == "__main__":
+	pass
